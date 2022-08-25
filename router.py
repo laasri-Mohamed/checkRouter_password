b907cfd825e03401fb09c621a3b74098f9daf740
laasri-Mohamed <laasrimed2001@gmail.com>
2022-08-25 17:31:44 +0100

    final version

--- router.py
+++ router.py
@@ -1,3 +1,19 @@
+from selenium import webdriver
+import os
+import time
+
+from selenium.webdriver.common import keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+#import random
+import string
+#import os
+#import time
+
+
+
 import random
 import webbrowser
 from tkinter import *
@@ -8,6 +24,10 @@ import csv
 from ttkbootstrap import *
 from datetime import datetime
 
+
+
+#login = driver.find_element(by=By.ID,value='LoginId')
+#login.click()
 
  
  
@@ -24,7 +44,7 @@ class window:
           'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q',
           'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
      
-    sym = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|',
+    sym = ['@', '#', '$', '%', '=', ':', '?', '.', '/',
            '~', '>', '*', '<']
      
     def __init__(self, root, geo, title) -> None:
@@ -32,22 +52,32 @@ class window:
         self.root.title(title)
         self.root.geometry(geo)
         self.root.resizable(width=False, height=False)
- 
-        Label(self.root, text='Your Password').grid(row=1, column=0, padx=10, pady=10)
-        Label(self.root, text='Date').grid(row=2, column=0, padx=10, pady=10)
-        Label(self.root, text='Url').grid(row=0, column=0, padx=10, pady=10)
+        Label(self.root, text='Username').grid(row=0, column=0, padx=10, pady=10)
+        Label(self.root, text='Password for login').grid(row=1, column=0, padx=10, pady=10)
+        Label(self.root, text='Your Password').grid(row=3, column=0, padx=10, pady=10)
+        Label(self.root, text='Date').grid(row=4, column=0, padx=10, pady=10)
+        Label(self.root, text='Gateway').grid(row=2, column=0, padx=10, pady=10)
+        
+        self.username = StringVar()
+        self.PASs = StringVar()
         self.pa = StringVar()
         self.date = StringVar()
         self.url = StringVar()
-        ttk.Entry(self.root, width=30, textvariable=self.pa
-                 ).grid(row=1, column=1, padx=4, pady=4)
-        ttk.Entry(self.root, width=30, textvariable=self.date
-                 ).grid(row=2, column=1, padx=4, pady=4)
-        ttk.Entry(self.root, width=30, textvariable=self.url
+        ttk.Entry(self.root, width=30, textvariable=self.username
                  ).grid(row=0, column=1, padx=4, pady=4)
+        ttk.Entry(self.root, width=30, textvariable=self.PASs
+                 ).grid(row=1, column=1, padx=4, pady=4)
+        ttk.Entry(self.root, width=30, textvariable=self.url
+                 ).grid(row=2, column=1, padx=4, pady=4)
+        ttk.Entry(self.root, width=30, textvariable=self.pa
+                 ).grid(row=3, column=1, padx=4, pady=4)
+        ttk.Entry(self.root, width=30, textvariable=self.date
+                 ).grid(row=4, column=1, padx=4, pady=4)
+
+
         self.length = StringVar()
- 
-        e = ttk.Combobox(self.root, values=['4', '8', '12', '16', '20', '24','28','32'],
+       
+        e = ttk.Combobox(self.root, values=['4', '8', '12', '16','18', '20', '24','28','32'],
                          textvariable=self.length)
         e.grid(row=0, column=2)
         e['state'] = 'readonly'
@@ -58,33 +88,40 @@ class window:
                    command=self.generate).grid(row=1, column=2)
          
         ttk.Button(self.root, text='Save to Database', style='success.TButton',
-                   width=20, padding=5, command=self.save).grid(row=3, column=2)
+                   width=20, padding=5, command=self.save).grid(row=5, column=2)
          
         ttk.Button(self.root, text='Delete', width=20, style='danger.TButton',
                    padding=5, command=self.erase).grid(row=2, column=2)
 
         ttk.Button(self.root, text='Dernier', width=20, style='success.TButton',
-                   padding=5, command=self.dernier).grid(row=3, column=0)
+                   padding=5, command=self.dernier).grid(row=5, column=0)
         
         ttk.Button(self.root, text='Search', width=20, style='danger.TButton',
-                   padding=5, command=self.search).grid(row=5, column=2)
+                   padding=5, command=self.search).grid(row=8, column=2)
          
         
          
         ttk.Button(self.root, text='Update', width=20, padding=5,
-                   command=self.update).grid(row=3, column=1)
+                   command=self.update).grid(row=5, column=1)
  
         # ========self.tree view=============
         self.tree = ttk.Treeview(self.root, height=5)
-        self.tree['columns'] = ('date', 'pas')
+        self.tree['columns'] = ('user','Pas', 'site', 'date', 'pas')
         self.tree.column('#0', width=0, stretch=NO)
+        self.tree.column('user', width=160, anchor=W)
+        self.tree.column('Pas', width=160, anchor=W)
+        self.tree.column('site', width=200, anchor=W)
         self.tree.column('date', width=160, anchor=W)
-        self.tree.column('pas', width=180, anchor=W)
+        self.tree.column('pas', width=160, anchor=W)
         self.tree.heading('#0', text='')
-        self.tree.heading('date', text='date name')
+        self.tree.heading('user', text='Username')
+        self.tree.heading('Pas', text='Password for login')
+        self.tree.heading('site', text='Gateway')
+        self.tree.heading('date', text='date')
         self.tree.heading('pas', text='Password')
+
  
-        self.tree.grid(row=4, column=0, columnspan=3, pady=10)
+        self.tree.grid(row=6, column=0, columnspan=3, pady=10)
         self.tree.bind("<ButtonRelease-1>", self.catch)
         #  cette commande appellera la fonction catch
  
@@ -138,12 +175,19 @@ class window:
         # cette fonction prendra toutes les données sélectionnées
         # e la table/arborescence et remplira le
         # champs de saisie respectifs
+        self.username.set('')
+        self.PASs.set('')
+        self.url.set('')
         self.pa.set('')
         self.date.set('')
         selected = self.tree.focus()
         value = self.tree.item(selected, 'value')
-        self.date.set(value[0])
-        self.pa.set(value[1])
+        self.username.set(value[0])
+        self.PASs.set(value[1])
+        self.url.set(value[2])
+        self.date.set(value[3])
+        self.pa.set(value[4])
+
  
     def update(self):
         # cette fonction mettra à jour la base de données avec de nouveaux
@@ -162,22 +206,25 @@ class window:
         else:
             for row in back.show():
                 self.tree.insert(parent='', text='', index='end',
-                                 values=(row[0], row[1]))
+                                 values=(row[0], row[1],row[2], row[3],row[4]))
  
     def erase(self):
         # cela supprimera ou supprimera le tuple sélectionné ou
         # ligne de la base de données
+        messagebox.showerror('Attention Amigo!', 'Do you want really to delete your data')
+        
         selected = self.tree.focus()
         value = self.tree.item(selected, 'value')
-        back.Del(value[1])
+        back.Del(value[4])
+        
         self.refresh()
- 
+
     def save(self):
         # cette fonction insèrera toutes les données dans le
         # base de données
-        back.enter(self.date.get(), self.pa.get())
+        back.enter(self.username.get(),self.PASs.get(), self.url.get(), self.date.get(), self.pa.get())
         self.tree.insert(parent='', index='end', text='',
-                         values=(self.date.get(), self.pa.get()))
+                         values=(self.username.get(),self.PASs.get(), self.url.get(),self.date.get(), self.pa.get()))
  
     def generate(self):
         # cette fonction produira une chaîne aléatoire qui
@@ -197,12 +244,20 @@ class window:
             self.date.set(b)
             mot_de_passe =a
             print("le nouveau mot de passe est: "+mot_de_passe)
-    
-    def dernier(self):
-        back.last()   
-    def search(self):
-        back.URL(self.url.get()) 
 
+
+    def dernier(self):
+        lp = back.last()
+        
+    def User(self):
+        back.USERNAME(self.username.get())
+        
+    def passWORD(self):
+        back.PASSWORD(self.PASs.get())
+    
+    def search(self):
+        back.URL(self.username.get(), self.PASs.get(),self.url.get())
+        
     def export(self):
         # cette fonction sauvegardera toutes les données du
         # base de données au format csv qui peut être ouvert
@@ -226,8 +281,16 @@ class window:
  
 if __name__ == '__main__':
     win = Style(theme='darkly').master
-    name = 'Password Generator'
-    dimension = '565x320'
+    name = 'ORANGE WIFI'
+    dimension = '850x400'
  
     app = window(win, dimension, name)
+    win.configure(bg='#F7EF8A')
     win.mainloop()
+ 
+
+
+    
+
+
+
